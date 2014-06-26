b62c780f62cfc47de7c2bdd10199f92915bdde00
Kevin <kevin@localhost.(none)>
2014-06-26 18:26:56 +0800

    app package view/index

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var upload = require('jquery-file-upload-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -20,9 +26,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
+
+
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/upload', upload.fileHandler({
+    uploadDir: __dirname + '/public/uploads',
+    uploadUrl: '/uploads',
+    imageVersions: {
+        thumbnail: {
+            width: 80,
+            height: 80
+        }
+    }
+}));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
